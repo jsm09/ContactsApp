29d5eec7ec85a38d8afa1cf1bf01e383d64ec05a
Laura Ware <lware@lauras-macbook-air.local>
2016-07-26 18:44:21 -0400

    Added the ability to edit the order of Contacts and delete contacts. Also added minor edits to names and phone numbers.

--- Contacts App/ContactsLessonTableViewController.swift
+++ Contacts App/ContactsLessonTableViewController.swift
@@ -12,7 +12,20 @@ class ContactsLessonTableViewController: UITableViewController {
     var contacts: [Contact] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.contacts = [Contact(firstName: "Joe", lastName: "Shmoe", phone: phoneNumber(areaCode: 123, firstThree: 456, lastFour: 7890)), Contact(firstName: "Jill", lastName: "Hanson", phone: phoneNumber(areaCode: 234, firstThree: 765, lastFour: 4321)), Contact(firstName: "Abe", lastName: "Larson", phone: phoneNumber(areaCode: 123, firstThree: 456, lastFour: 7890)), Contact(lastName: "Shang", phone: phoneNumber(areaCode: 234, firstThree: 563, lastFour: 1234))]
+        self.contacts = [Contact(firstName: "Joe", lastName: "Shmoe"), Contact(firstName: "Jill", lastName: "Hanson", phone: phoneNumber(areaCode: 234, firstThree: 765, lastFour: 4321)), Contact(firstName: "Abe", phone: phoneNumber(areaCode: 123, firstThree: 456, lastFour: 7890)), Contact(lastName: "Shang", phone: phoneNumber(areaCode: 234, firstThree: 563, lastFour: 1234))]
+        
+        let moveButton = UIBarButtonItem(title: "Edit", style: .Plain, target: self, action: Selector("toggleEdit"))
+        //let doneButton = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: Selector("toggleEdit"))
+        
+        self.navigationItem.leftBarButtonItem = moveButton
+/*
+        if tableView.editing {
+        }
+        else {
+            self.navigationItem.leftBarButtonItem = nil
+            self.navigationItem.leftBarButtonItem = moveButton
+        }
+        */
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -20,6 +33,34 @@ class ContactsLessonTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
+    func toggleEdit() {
+        print(self.navigationItem.leftBarButtonItem!.title)
+        print("Editing Mode" + String(tableView.editing))
+        if tableView.editing {
+            print(self.navigationItem.leftBarButtonItem!.title)
+            self.navigationItem.leftBarButtonItem!.title = "Edit"
+            print(self.navigationItem.leftBarButtonItem!.title)
+
+        }
+        else {
+            self.navigationItem.leftBarButtonItem!.title = "Done"
+        }
+        self.tableView.setEditing(!tableView.editing, animated: true)
+
+    }
+    
+    override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        if tableView.editing {
+            return .None
+        } else {
+            return .Delete
+        }
+    }
+    
+    override func tableView(tableView: UITableView, shouldIndentWhileEditingRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -48,32 +89,36 @@ class ContactsLessonTableViewController: UITableViewController {
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            self.contacts.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
-    /*
+    
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+        
+        let contactMoving = contacts.removeAtIndex(fromIndexPath.row)
+        contacts.insert(contactMoving,atIndex: toIndexPath.row)
 
     }
-    */
+    
 
     /*
     // Override to support conditional rearranging of the table view.
